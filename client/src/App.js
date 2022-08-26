4c47e7ae8603082f89b8ace9d34c0009ddf262f7
v4ndr <romain.vdp@gmail.com>
2022-08-26 16:12:07 +0200

    v0.0.1

--- client/src/App.js
+++ client/src/App.js
@@ -6,57 +6,77 @@ import adventure from './assets/adventure.png';
 import sad from './assets/sad.png';
 import tension from './assets/tension.png';
 import fight from './assets/fight.png';
+import './App.css';
 
 function App() {
-  const [mood, setMood] = React.useState('adventure');
+  const [mood, setMood] = React.useState();
+  // eslint-disable-next-line no-unused-vars
+  const [playState, setPlayState] = React.useState(false);
   const media = React.useRef(null);
   const url = `http://localhost:3001/stream?mood=${mood}`;
 
+  const audio = document.querySelector('audio');
+
+  const onPPClick = () => {
+    if (playState) {
+      audio.pause();
+      setPlayState(false);
+    } else {
+      audio.play();
+      setPlayState(true);
+    }
+  };
+
   return (
-    <div style={styles.container}>
+    <div id="container">
       <h1 style={styles.title}>OPMB</h1>
       <h2 style={styles.subtitile}>Because every mood deserves an approriate One Piece OST</h2>
       <div style={styles.moodsContainer}>
-        <MoodButton mood="fight" setMood={setMood} img={fight} containerStyle={styles.moodBtn} />
-        <MoodButton mood="fun" setMood={setMood} img={fun} containerStyle={styles.moodBtn} />
-        <MoodButton mood="adventure" setMood={setMood} img={adventure} containerStyle={styles.moodBtn} />
-        <MoodButton mood="sad" setMood={setMood} img={sad} containerStyle={styles.moodBtn} />
-        <MoodButton mood="tension" setMood={setMood} img={tension} containerStyle={styles.moodBtn} />
+        <MoodButton mood="fight" setMood={setMood} img={fight} activeMood={mood} />
+        <MoodButton mood="fun" setMood={setMood} img={fun} activeMood={mood} />
+        <MoodButton mood="adventure" setMood={setMood} img={adventure} activeMood={mood} />
+        <MoodButton mood="sad" setMood={setMood} img={sad} activeMood={mood} />
+        <MoodButton mood="tension" setMood={setMood} img={tension} activeMood={mood} />
       </div>
-      <audio controls ref={media} src={url} autoPlay />
+      <audio hidden controls ref={media} src={url} autoPlay onPlay={() => setPlayState(true)} />
+      <button style={styles.pp} type="button" onClick={onPPClick}>{playState ? 'PAUSE' : 'PLAY'}</button>
     </div>
   );
 }
 
 const styles = {
-  container: {
-    flexDirection: 'column',
-    backgroundColor: '#2E2E2E',
-    display: 'flex',
-    flex: 1,
-    height: '100vh',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   title: {
     color: 'white',
     fontSize: 44,
     fontFamily: 'Noto Sans, sans-serif',
-    margin: 0,
-    marginBottom: 15,
+    margin: 15,
+    textAlign: 'center',
   },
   subtitile: {
     color: 'white',
     fontSize: 28,
     fontFamily: 'Noto Sans, sans-serif',
     fontWeight: 300,
-    margin: 0,
+    margin: 15,
+    marginTop: 0,
     textAlign: 'center',
   },
   moodsContainer: {
+    width: '100%',
     display: 'flex',
+    flexWrap: 'wrap',
     flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 42,
+  },
+  pp: {
+    marginTop: 40,
+    fontSize: 22,
+    padding: 10,
+    borderRadius: 10,
+    border: 'none',
+    width: 110,
   },
 };
 
--- client/src/components/MoodButton.js
+++ client/src/components/MoodButton.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function MoodButton({ mood, setMood, img }) {
+function MoodButton({
+  mood, setMood, img, activeMood,
+}) {
   function onClick() {
     setMood(mood);
   }
@@ -9,7 +11,18 @@ function MoodButton({ mood, setMood, img }) {
   return (
     /* eslint-disable-next-line
     jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
-    <img src={img} alt={mood} style={styles.btn} onClick={(onClick)} />
+    <img
+      src={img}
+      alt={mood}
+      style={{
+        flex: 'auto',
+        margin: 6,
+        objectFit: 'contain',
+        maxWidth: 150,
+        filter: activeMood === mood ? null : 'grayscale(1)',
+      }}
+      onClick={(onClick)}
+    />
   );
 }
 
@@ -17,13 +30,7 @@ MoodButton.propTypes = {
   mood: PropTypes.string.isRequired,
   setMood: PropTypes.func.isRequired,
   img: PropTypes.string.isRequired,
-};
-
-const styles = {
-  btn: {
-    height: 130,
-    margin: 6,
-  },
+  activeMood: PropTypes.string.isRequired,
 };
 
 export default MoodButton;
